0b1d2112027ded06d9a54a41d85ca91040509876
1212Hamida <97199401+1212Hamida@users.noreply.github.com>
2022-01-05 21:48:15 -0800

    Update ThreeClass.java

--- GitPractice3/src/ThreeClass.java
+++ GitPractice3/src/ThreeClass.java
@@ -2,7 +2,17 @@
 public class ThreeClass {
 
 	public static void main(String[] args) {
-		System.out.println("Still no code!");
+	int [] num = {2,3,4,5,6,7};
+	int sumResult = 0;
+	double multiResult=0;
+	
+		for (int i=0;i<num.length;i++){
+			sumresult += num[i];
+			multiResult *= num[i];
+		System.out.println("The sum of numbers are: "+sumresult);
+		System.out.printlin("The multiplies of numbers are: "+multiResult);
+		}	
+		
 
 	}
 
